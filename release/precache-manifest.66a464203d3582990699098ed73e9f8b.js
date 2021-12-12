f78654e2f2ebd300621e4669dd99acdf492150aa
akivab <akiva.bamberger@gmail.com>
2021-12-12 00:56:07 -0800

    publish release

---
+++ release/precache-manifest.66a464203d3582990699098ed73e9f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b07d5acf5863fa807afb0ce4e22a58",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9cbb49b8153cc854608c",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e0b8ddd39990682aff4",
+    "url": "./static/js/2.b18d10f2.chunk.js"
+  },
+  {
+    "revision": "84f82ff8fcbda2312ccc7a85f90c212d",
+    "url": "./static/js/2.b18d10f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cbb49b8153cc854608c",
+    "url": "./static/js/main.ab1df716.chunk.js"
+  },
+  {
+    "revision": "607911425784431aa9f5",
+    "url": "./static/js/runtime-main.aef6d470.js"
+  }
+]);
